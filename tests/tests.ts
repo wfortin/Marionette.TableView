385f9f02d02fdc59ad3457f0f969595a394ce278
wfortin <willyfortin@gmail.com>
2015-05-03 22:56:22 -0400

    + Initial setup for karma and jasmine unit tests

---
+++ tests/tests.ts
@@ -0,0 +1,8 @@
+/// <reference path="../_definitions.d.ts" />
+/// <reference path="../definitions/jasmine.d.ts" />
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
